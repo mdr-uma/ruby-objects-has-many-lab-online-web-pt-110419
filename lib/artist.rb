fd47a8bc62ae283c0ead204a247008600644b3e1
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-13 21:15:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   attr_accessor :name
 
-  @@song_count = 0
+  # @@song_count = 0
 
   def songs
     Song.all.select {|song| song.artist == self}
@@ -23,7 +23,7 @@ class Artist
    def add_song(song)
      @songs << song
      song.artist = self
-     @@song_count += 1
+    #  @@song_count += 1
    end
 
    def add_song_by_name(name)
