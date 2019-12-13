2e7bff1b1249bfea1b08b644cc19bbf9c3ef3dc1
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-13 20:35:27 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def songs
+    #Song.all.select {|song| song.artist == self}
+    Song.all.each do |song|
+      if song.artist == self
+        @songs << song
+      end
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @songs = []
+   end
+
+   def add_song(song)
+     @songs << song
+     song.artist = self
+     @@song_count += 1
+   end
+
+   def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+   end
+
+   def self.song_count
+     @@song_count
+   end
+
+end
